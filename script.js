bc7ccfd6fd3d1d32992192df3687761f69b17646
Ayomal Praveen <ayomalpraveen33@gmail.com>
2020-11-01 16:58:32 +0530

    made the object works

--- script.js
+++ script.js
@@ -1,28 +1,41 @@
-document.addEventListener('DOMContentLoaded',()=>{
-    const dino=document.querySelector(".dinasour");
-    document.addEventListener("keyup",control);
-
-    //adding the control function for both space & up-arrow key
-    function control(e){
-        if(e.keyCode == '38' || e.keyCode == '32'){
-            jump()
-        }
+document.addEventListener("DOMContentLoaded", () => {
+  const dino = document.querySelector(".dinasour");
+  document.addEventListener("keyup", control);
+  let isJump = false;
+  let gravity = 0.9;
+  //adding the control function for both space & up-arrow key
+  function control(e) {
+    if (e.keyCode == "38" || e.keyCode == "32") {
+      if (!isJump) {
+        isJump = true;
+        jump();
+      }
     }
-
-    function jump(){
-        let position=0
-        let timerID=setInterval(()=>{
-            //move down
-            if(position === 150){
-                clearInterval(timerID)
-                position -=30;
-                dino.style.bottom=position+'px'
-            }
-            //move up
-            position +=30
-            dino.style.bottom=position + 'px';
-        },20)
-        
-        
-    }
-})
+  }
+  let position = 0;
+  function jump() {
+    let count = 0;
+    let timerID = setInterval(() => {
+      //move down
+      if (count === 10) {
+        clearInterval(timerID);
+        let downTimer = setInterval(function () {
+          if (count === 0) {
+            clearInterval(downTimer);
+            isJump = false;
+            console.log(position);
+          }
+          count--;
+          position -= 8;
+          position = position * gravity;
+          dino.style.bottom = position + "px";
+        }, 30);
+      }
+      //move up
+      count++;
+      position += 30;
+      position = position * gravity;
+      dino.style.bottom = position + "px";
+    }, 30);
+  }
+});
